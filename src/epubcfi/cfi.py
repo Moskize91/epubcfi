4a0d92140c5790360609c475fc31123ee096676a
TaoZeyu <i@taozeyu.com>
2025-01-21 15:44:55 +0800

    feat(src): complete parse cfi logic

--- src/epubcfi/cfi.py
+++ src/epubcfi/cfi.py
@@ -1,15 +1,32 @@
 import re
+from .parser import parse, Path
 
 class EpubCFI:
   pass
 
-def parse(path: str):
-  cfi = _capture_cfi(path)
+def split(path: str) -> tuple[str, None | Path | tuple[Path, Path, Path]]:
+  tail, cfi = _capture_cfi(path)
   if cfi is None:
-    return
-  
+    return path, None
+  result = parse(cfi)
+  prefix = path[:len(path) - len(tail)]
+  return prefix, result
+
+def to_absolute(paths: tuple[Path, Path, Path]) -> tuple[Path, Path]:
+  parent, start0, end0 = paths
+  start = Path(
+    steps=parent.steps + start0.steps,
+    offset=start0.offset,
+  )
+  end = Path(
+    steps=parent.steps + end0.steps,
+    offset=end0.offset,
+  )
+  return start, end
   
 def _capture_cfi(path: str):
   matched = re.search(r"(#|^)epubcfi\((.*)\)$", path)
   if matched:
-    return matched.group(2)
+    return matched.group(), matched.group(2)
+  else:
+    return None, None
--- tests/test_cfi.py
+++ tests/test_cfi.py
@@ -1,18 +1,27 @@
 import unittest
-from src.epubcfi.cfi import _capture_cfi
+from src.epubcfi.cfi import split, to_absolute, _capture_cfi
 
 class TestChunk(unittest.TestCase):
 
+  def test_to_absolute(self):
+    prefix, results = split("book.epub#epubcfi(/6/4,!/2[foobar],/10/4[foz])")
+    self.assertTrue(isinstance(results, tuple))
+    start, end = to_absolute(results)
+    self.assertEqual(prefix, "book.epub")
+    self.assertEqual(str(start), "/6/4!/2[foobar]")
+    self.assertEqual(str(end), "/6/4/10/4[foz]")
+
   def test_capture_cfi(self):
-    self.assertEqual(
-      _capture_cfi("book.epub#epubcfi(/6/4[chap01ref]!/4[body01]/10[para05]/3:10)"),
+    pairs = [(
+      "book.epub#epubcfi(/6/4[chap01ref]!/4[body01]/10[para05]/3:10)",
       "/6/4[chap01ref]!/4[body01]/10[para05]/3:10",
-    )
-    self.assertEqual(
-      _capture_cfi("epubcfi(/6/4[chap01ref]!/4[body01]/10[para05]/3:10)"),
+    ), (
+      "epubcfi(/6/4[chap01ref]!/4[body01]/10[para05]/3:10)",
       "/6/4[chap01ref]!/4[body01]/10[para05]/3:10",
-    )
-    self.assertEqual(
-      _capture_cfi("epubcfi(/6/4[chap01ref]!/4[body01]/10[para05]/3:10)foobar"),
+    ), (
+      "epubcfi(/6/4[chap01ref]!/4[body01]/10[para05]/3:10)foobar",
       None,
-    )
+    )]
+    for source, target in pairs:
+      _, cfi = _capture_cfi(source)
+      self.assertEqual(cfi, target)
