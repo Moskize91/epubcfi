4531cf7edb9a459facff2d081e1ace545fd7bf2e
TaoZeyu <i@taozeyu.com>
2025-01-21 17:10:29 +0800

    fix(src): cannot pass 0 offset

--- tests/test_history_bug.py
+++ tests/test_history_bug.py
@@ -6,6 +6,7 @@ class TestHistoryBug(unittest.TestCase):
   def test_parse_and_compare(self):
     history_expressions = [
       "epubcfi(/6/24[id18]!/4/422/2/3,:227,:228)",
+      "epubcfi(/6/22[id17]!/4/62/1,:0,:59)"
     ]
     for expression in history_expressions:
       _, path = split(expression)
