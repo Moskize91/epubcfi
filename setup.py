563fc7770f68edf542e0cb95bb5fe97fb69f91a3
TaoZeyu <i@taozeyu.com>
2025-01-24 14:50:44 +0800

    chore(project): cannot release packages

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
   name="epubcfi",
-  version="0.0.2",
+  version="0.0.3",
   author="Tao Zeyu",
   author_email="i@taozeyu.com",
   description="handle Epub CFI",
-  packages=["epubcfi"],
+  packages=find_packages(),
   long_description=open("./README.md", encoding="utf8").read(),
   long_description_content_type="text/markdown",
   install_requires=[],
